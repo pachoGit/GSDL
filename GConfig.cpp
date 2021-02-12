7976f00ee291e45219737304fb5aef156cb64949
pachoGit <carlos.hcpv@gmail.com>
2021-02-12 07:42:12 -0500

    Fuente de texto predeterminado

--- GConfig.cpp
+++ GConfig.cpp
@@ -1,4 +1,5 @@
 #include "GConfig.h"
 
-string GConfig::nombre_fuente = "";
+string GConfig::nombre_fuente = "OpenSans-Regular.ttf";
+string GConfig::path_fuente = "recursos/fuentes/open-sans/";
 unsigned GConfig::tam_fuente = 10;
--- GConfig.h
+++ GConfig.h
@@ -10,6 +10,9 @@ class GConfig
     // Fuente por defecto de todos los textos
     static string nombre_fuente;
 
+    // Ruta de la fuente de texto
+    static string path_fuente;    
+
     // Tamanio de fuente por defecto de todos los textos
     static unsigned tam_fuente;
 };
